2238723bf0727152cb12d673358eff0ca4e32475
Navika123 <79006185+Navika123@users.noreply.github.com>
2021-06-27 19:53:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+create ground class
+stand class
+
+
+
+function preload()
+
+var block1
+block2
+block3
+v\block4
+block5
+block6
+bllock7
+block8
+block9
+block10
+block11
+block12
+
+function setup()
+
+
+
+function draw()
